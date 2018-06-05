026bf6f5d9630d7bc4203ae20213329c19bcdcd7
Ashish Vishwakarma <Hello@AshishVishwakarma.com>
2018-06-06 02:48:03 +0530

    Add project files.

---
+++ XrmToolBox.WhoAmIPlugin/MyPlugin.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Composition;
+using XrmToolBox.Extensibility;
+using XrmToolBox.Extensibility.Interfaces;
+
+namespace XrmToolBox.WhoAmIPlugin
+{
+    // Do not forget to update version number and author (company attribute) in AssemblyInfo.cs class
+    // To generate Base64 string for Images below, you can use https://www.base64-image.de/
+    [Export(typeof(IXrmToolBoxPlugin)),
+        ExportMetadata("Name", "My First Plugin"),
+        ExportMetadata("Description", "This is a description for my first plugin"),
+        // Please specify the base64 content of a 32x32 pixels image
+        ExportMetadata("SmallImageBase64", null),
+        // Please specify the base64 content of a 80x80 pixels image
+        ExportMetadata("BigImageBase64", null),
+        ExportMetadata("BackgroundColor", "Lavender"),
+        ExportMetadata("PrimaryFontColor", "Black"),
+        ExportMetadata("SecondaryFontColor", "Gray")]
+    public class MyPlugin : PluginBase
+    {
+        public override IXrmToolBoxPluginControl GetControl()
+        {
+            return new MyPluginControl();
+        }
+    }
+}
